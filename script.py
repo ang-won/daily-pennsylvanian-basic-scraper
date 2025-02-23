be8e6aef573d2aa33ff2467054b1a4a5f083dfde
Angelina Wong <wongang@seas.upenn.edu>
2025-02-22 22:13:14 -0500

    Update script.py to scrape top headline from Opinion section of DP

--- script.py
+++ script.py
@@ -15,26 +15,28 @@ import loguru
 
 def scrape_data_point():
     """
-    Scrapes the main headline from The Daily Pennsylvanian home page.
+    Scrapes the top article headline from The Daily Pennsylvanian Opinion section.
 
     Returns:
         str: The headline text if found, otherwise an empty string.
     """
     headers = {
-    "User-Agent": "cis3500-scraper"
+        "User-Agent": "cis3500-scraper"
     }
     
-    req = requests.get("https://www.thedp.com", headers=headers)
+    req = requests.get("https://www.thedp.com/section/opinion", headers=headers)
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link")
-        data_point = "" if target_element is None else target_element.text
+        target_element = soup.find("h3", class_="headline")
+        
+        data_point = "" if target_element is None else target_element.text.strip()
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
 
+    return ""
 
 if __name__ == "__main__":
 
